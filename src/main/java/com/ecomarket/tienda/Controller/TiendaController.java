b5b260735e923d9b5563adc2f97587be4b119298
Orlando <169166413+OrlandoDiaz2003@users.noreply.github.com>
2025-05-18 18:16:56 -0400

    Orlando: Cree los packages junto con sus files (#1)
    * Se agregan packages y sus clases y interface de tienda
    
    * Se agregan packages y sus clases y interface de tienda

---
+++ src/main/java/com/ecomarket/tienda/Controller/TiendaController.java
@@ -0,0 +1,5 @@
+package com.ecomarket.tienda.Controller;
+
+public class TiendaController {
+
+}
---
+++ src/main/java/com/ecomarket/tienda/Repository/TiendaRespository.java
@@ -0,0 +1,9 @@
+package com.ecomarket.tienda.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecomarket.tienda.Model.Tienda;
+
+public interface TiendaRespository extends JpaRepository<Tienda, Integer>{
+
+}
